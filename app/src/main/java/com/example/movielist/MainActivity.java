8bc3d1ca1a64f305d35dbca626c5eb6a2ff83f13
TejasPatel <tjp083@gmail.com>
2020-11-13 02:33:34 -0500

    movie details addded into the ArrayList

--- app/src/main/java/com/example/movielist/MainActivity.java
+++ app/src/main/java/com/example/movielist/MainActivity.java
@@ -17,5 +17,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.movie_listView);
+
+        arrayList = new ArrayList<movie>();
+
+
+        arrayList.add(new movie("Dil Bechara","7.9 IMDB","Romantic Movie"));
+        arrayList.add(new movie("Laxmi Bomb","2.2 IMDB","Horror Movie"));
+        arrayList.add(new movie("Sooryavanshi","coming soon","Action Movie"));
+        arrayList.add(new movie("Avengers","8.4 IMDB","Action Movie"));
+        arrayList.add(new movie("War","8.2 IMDB","Action Movie"));
+        arrayList.add(new movie("Civil War","7.8 IMDB","Action Movie"));
+        arrayList.add(new movie("3 idiots","8.4 IMDB","Comedy Movie"));
+        arrayList.add(new movie("Raees","6.8 IMDB","Crime Movie"));
+        arrayList.add(new movie("Baaghi 3","2.1 IMDB","Action Movie"));
+        arrayList.add(new movie("Phir Hera Pheri","6.9 IMDB","Comedy Movie"));
+
+        adapterList = new CustomAdapterList(arrayList);
+
+        listView.setAdapter(adapterList);
     }
 }
