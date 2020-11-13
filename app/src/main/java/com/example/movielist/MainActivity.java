1e91e8c145364986c614190ea71043e8ee87210d
TejasPatel <tjp083@gmail.com>
2020-11-13 02:42:14 -0500

    UI is updated

--- app/src/main/java/com/example/movielist/MainActivity.java
+++ app/src/main/java/com/example/movielist/MainActivity.java
@@ -23,16 +23,16 @@ public class MainActivity extends AppCompatActivity {
         arrayList = new ArrayList<movie>();
 
 
-        arrayList.add(new movie("Dil Bechara","7.9 IMDB","Romantic Movie"));
-        arrayList.add(new movie("Laxmi Bomb","2.2 IMDB","Horror Movie"));
-        arrayList.add(new movie("Sooryavanshi","coming soon","Action Movie"));
-        arrayList.add(new movie("Avengers","8.4 IMDB","Action Movie"));
-        arrayList.add(new movie("War","8.2 IMDB","Action Movie"));
-        arrayList.add(new movie("Civil War","7.8 IMDB","Action Movie"));
-        arrayList.add(new movie("3 idiots","8.4 IMDB","Comedy Movie"));
-        arrayList.add(new movie("Raees","6.8 IMDB","Crime Movie"));
-        arrayList.add(new movie("Baaghi 3","2.1 IMDB","Action Movie"));
-        arrayList.add(new movie("Phir Hera Pheri","6.9 IMDB","Comedy Movie"));
+        arrayList.add(new movie("Dil Bechara","7.9 IMDB","Romantic"));
+        arrayList.add(new movie("Laxmi Bomb","2.2 IMDB","Horror"));
+        arrayList.add(new movie("Sooryavanshi","coming soon","Action"));
+        arrayList.add(new movie("Avengers","8.4 IMDB","Action"));
+        arrayList.add(new movie("War","8.2 IMDB","Action"));
+        arrayList.add(new movie("Civil War","7.8 IMDB","Action"));
+        arrayList.add(new movie("3 idiots","8.4 IMDB","Comedy"));
+        arrayList.add(new movie("Raees","6.8 IMDB","Crime"));
+        arrayList.add(new movie("Baaghi 3","2.1 IMDB","Action"));
+        arrayList.add(new movie("Phir Hera Pheri","6.9 IMDB","Comedy"));
 
         adapterList = new CustomAdapterList(arrayList);
 
